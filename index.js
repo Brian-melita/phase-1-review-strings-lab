51fdcacba4e8b5da771f3b92909499ecc85f19d9
Brian-melita <brianmelitah@gmail.com>
2025-09-17 06:39:01 +0300

    Reviewed Strings

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const currentUser = "Grace Hopper"
 
 const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`
 
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
-const shortGreeting = "Welcome, G !"
-
-
-
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
